fdd6ea377b4ec0d2eac51f898734a03face207d8
dipali8111999 <dipalichavai12@gmail.com>
2025-12-25 15:24:07 +0530

    Theme switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import { Employee } from './components/Employee.jsx'
-import Login from './components/Login.jsx'
-import Profile from './components/Profile.jsx'
-import UserContextProvider from './context/UserContextProvider.jsx'
+import { ThemeProvider } from './context/ThemeContext'
+import ThemeButton from './components/ThemeButton'
+import Card from './components/Card'
 
 function App() {
+  const [themeMode, setThemeMode] = useState('light');
+  const setLightTheme = () => {
+    setThemeMode('light');
+  }
+  const setDarkTheme = () => {
+    setThemeMode('dark');
+  }
+
+  useEffect(() => {
+    const elment = document.querySelector('html');
+    elment.classList?.remove("light", "dark");
+    elment?.classList?.add(themeMode);
+  }, [themeMode])
+
   return (
-    <>
-      <UserContextProvider>
-        <Login/>
-        <Profile/>
-      </UserContextProvider>
-      {/* <Employee/> */}
-    </>
+    <ThemeProvider value={{themeMode, setDarkTheme, setLightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeButton/>
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
